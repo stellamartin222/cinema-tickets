a2a133993f8e713631ecfcb1f868e0f91b79fe11
Stella Martin <stellamartin222@gmail.com>
2025-07-10 21:52:58 +0100

    refactor: moved creation of class into before each

--- cinema-tickets-javascript/test/src/pairtest/TicketService.test.js
+++ cinema-tickets-javascript/test/src/pairtest/TicketService.test.js
@@ -1,6 +1,12 @@
 import TicketService from '../../../src/pairtest/TicketService';
 
 describe('TicketService', () => {
+    let ticketService;
+
+    beforeEach(async () => {
+        ticketService = new TicketService;
+    });
+
     it('will return success object on successful ticket purchase', () => {
         expect(ticketService.purchaseTickets(1234, [{ADULT: 1}])).toEqual({status: 201, message: 'Thank you for your order.'});
     });
