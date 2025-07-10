10c25d70d3cb46c490a35139f99731508c2f9315
Stella Martin <stellamartin222@gmail.com>
2025-07-10 21:49:24 +0100

    fix: fixed test by adding params

--- cinema-tickets-javascript/test/src/pairtest/TicketService.test.js
+++ cinema-tickets-javascript/test/src/pairtest/TicketService.test.js
@@ -2,7 +2,6 @@ import TicketService from '../../../src/pairtest/TicketService';
 
 describe('TicketService', () => {
     it('will return success object on successful ticket purchase', () => {
-        let ticketService = new TicketService;
-        expect(ticketService.purchaseTickets()).toEqual({status: 201, message: 'Thank you for your order.'});
+        expect(ticketService.purchaseTickets(1234, [{ADULT: 1}])).toEqual({status: 201, message: 'Thank you for your order.'});
     });
 });
